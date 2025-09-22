12d97b3e48d7d813db85c0259153cb43b081af31
Mateus Tenório <mateus.tenorio.ext@xcelis.com.br>
2025-09-22 12:19:48 -0300

    Refinamento

--- test/specs/forms.spec.js
+++ test/specs/forms.spec.js
@@ -3,8 +3,6 @@ const AppScreen = require('../../screen-objects/AppScreen');
 const formsData = require('../../data/forms_data.json');
 
 describe('Cenário de Formulários', () => {
-    allure.epic('Formulários');
-    allure.feature('Inputs');
   beforeEach(async () => {
     await AppScreen.formsTab.click();
     await FormsScreen.waitForIsShown();
@@ -12,27 +10,23 @@ describe('Cenário de Formulários', () => {
   
   formsData.forEach((data) => {
     it(`deve preencher o campo de texto com "${data.text}" e verificar o resultado`, async () => {
-      allure.story('Cenário de Validação');
       await FormsScreen.textInput.setValue(data.text);
       await expect(FormsScreen.inputTextResult).toHaveText(data.expected_result);
     });
   });
 
   it('deve alternar a chave seletora e verificar o estado', async () => {
-    allure.story('Cenário de Interação');
     await expect(FormsScreen.switchText).toHaveText('Click to turn the switch ON');
     await FormsScreen.switch.click();
     await expect(FormsScreen.switchText).toHaveText('Click to turn the switch OFF');
   });
 
   it('deve selecionar uma opção no dropdown', async () => {
-    allure.story('Cenário de Interação');
     const optionToSelect = 'Appium is awesome';
     await FormsScreen.selectDropdownOption(optionToSelect);
   });
   
   it('deve exibir um pop-up ao clicar no botão "Active"', async () => {
-    allure.story('Cenário de Interação');
     await FormsScreen.activeBtn.click();
     
     const alertTitle = await $('//android.widget.TextView[@resource-id="android:id/alertTitle"]');
